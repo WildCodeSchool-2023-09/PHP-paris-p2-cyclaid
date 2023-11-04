6ae5ec9e921047923ba81a2ea1dd2444f1bfbd3b
NicoDestephen <ndestephen@gmail.com>
2023-11-04 16:46:53 +0100

    update PartController and PartManager with const class + add DB cyclaid

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -6,6 +6,11 @@ use App\Model\PartManager;
 
 class PartController extends AbstractController
 {
+    public const EXTENTIONS = ['jpg','png', 'jpeg', 'webp'];
+    public const MAX_FILE_SIZE = 10000000;
+    public const MAX_LENGTH_DESCRIPTION = 2000;
+    public const MAX_LENGTH_TITLE = 250;
+    public const MAX_LENGTH_REFERENCE = 100;
     public function add()
     {
         $errors = [];
@@ -13,21 +18,21 @@ class PartController extends AbstractController
             $uploadDir = 'uploads/';
             $uploadFile = $uploadDir . uniqid() . basename($_FILES['file']['name']);
             $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-            $authorizedExtensions = ['jpg','png', 'jpeg', 'webp'];
-            $maxFileSize = 10000000;
+            $authorizedExtensions = self::EXTENTIONS;
+            $maxFileSize = self::MAX_FILE_SIZE;
 
             $data = array_map('trim', $_POST);
             $data = array_map('htmlentities', $data);
             if (empty($data['title'])) {
                 $errors['title'] = "A title is required";
             }
-            if (strlen($data['title']) > 250) {
+            if (strlen($data['title']) > self::MAX_LENGTH_TITLE) {
                 $errors['title'] = "The title is too long. Max 250 characters";
             }
             if (empty($data['reference'])) {
                 $errors['reference'] = "A reference is required";
             }
-            if (strlen($data['reference']) > 100) {
+            if (strlen($data['reference']) > self::MAX_LENGTH_REFERENCE) {
                 $errors['reference'] = "The reference is too long. Max 100 characters";
             }
             if (empty($data['category'])) {
@@ -45,7 +50,7 @@ class PartController extends AbstractController
             if (empty($data['description'])) {
                 $errors['description'] = "A description is required";
             }
-            if (strlen($data['description']) > 2000) {
+            if (strlen($data['description']) > self::MAX_LENGTH_DESCRIPTION) {
                 $errors['description'] = "The description is too long. Max 2000 characters";
             }
             if (empty($data['file'])) {
@@ -59,10 +64,10 @@ class PartController extends AbstractController
             }
             if (empty($errors)) {
                 move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile);
-                $data = $_POST;
-                $partsManager = new PartManager();
-                if ($partsManager->insert($data)) {
-                    return $this->add();
+                $partManager = new PartManager();
+                $part = $partManager->insert($data);
+                if ($part) {
+                    header('Location:/Home/index.html.twig');
                 }
             }
         }
--- src/Model/PartManager.php
+++ src/Model/PartManager.php
@@ -7,10 +7,18 @@ use App\Model\AbstractManager;
 class PartManager extends AbstractManager
 {
     public const TABLE = 'post';
+    public const CATEGORY = ['Accesories', 'Brakes', 'Cables and sheaths', 'Frames', 'Saddles',
+                             'Tools', 'Forks and steering', 'Wheels and tires', 'Transmission'];
+    public const WEAR = ['New', 'Good', 'Used', 'To fix'];
+    public const BRAND = ['Shimano', 'Hutchinson', 'Brooks', 'Continental', 'Schwalbe', 'Magura', 'Brompton', 'Other'];
+    public const LOCATION = ['Auverge', 'Rhône-Alpes', 'Bourgogne', 'Frnche-Comté',
+                             'Bretagne', 'Centre', 'Val de Loire', 'Corse', 'Grand Est',
+                             'Hauts de France', 'Ile de France', 'Normandie', 'Nouvelle Acquitaine',
+                             'Occitanie', 'Pays de la Loire', 'Provnce', 'Alpes', 'Côte d\'Azur', 'Undefined'];
 
     public function insert(array $data)
     {
-        $query = "INSERT INTO post (title, reference, category, wear, brand, location, description, file)";
+        $query = "INSERT INTO post (title, reference, category, wear, brand, location, description, file) ";
         $query .= " VALUES (:title, :reference, :category, :wear, :brand, :location, :description, :file);";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue(':title', $data['title']);
