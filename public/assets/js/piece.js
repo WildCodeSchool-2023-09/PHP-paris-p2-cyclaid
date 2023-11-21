c2d9e67e0f735d8cc340e53539db7bac6c8da155
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2023-11-21 10:25:37 +0100

    [UPDATE] post view and css

--- public/assets/js/piece.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    let popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-    console.log('eee');
-}
-
-let popupTrigger = document.getElementById("popupTrigger");
-
-popupTrigger.addEventListener('click', myFunction);
